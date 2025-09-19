5657ab71511fb6e6176dc3ff5b53c595764e5049
may-hatch <k025c2090@g.neec.ac.jp>
2025-09-19 10:04:37 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 """
 ランク戦ban管理
 作成日：2025/09/03
+更新日(1)：2025/09/19
 """
 import streamlit as st
 from supabase import create_client
@@ -13,6 +14,11 @@ url=os.getenv("SUPABASE_URL")
 key=os.getenv("SUPABASE_KEY")
 supabase=create_client(url,key)
 
+#マップ管理
+st.session_state["spawn_h"]=[0]
+st.session_state["spawn_s"]=[0,0,0,0]
+
+
 #アプリ作成
 #アプリ名
 st.title("ランク戦BANリスト")
@@ -22,7 +28,7 @@ with st.expander("使い方"):
     テストで作った身内用です！！サーバー外に共有しないでください！！
     なにか追加したい項目などあれば気軽に教えてください～
     
-    五段以上の３BANを想定した記録フォームです。
+    五段以上の鯖３BANを想定した記録フォームです。
     遭遇時の段位、BANされたキャラ、マップ、ハンターを選択してください。
     ※BANしたハンターは任意記録です。
     
@@ -33,35 +39,57 @@ with st.expander("使い方"):
 
     今はただ一覧を表示するようになってますが、
     もしデータが集まったらマップなどを含めた確率順表示とかしようと思います
+    
+    更新履歴：
+    2025-09-19
+    データ収集の進行に伴い更新。
+    検索結果から１キャラ一致を削除
+    マップを含めて優先順を付けた検索に仕様変更
+    記録項目（任意）にスポーン選択を追加
     """)
 
 #入力フォーム_段位
 rank=st.selectbox("段位を選択",options=["5","6","7"])
 
-#入力フォーム_サバイバー
-survivors={0: '', 1: '医師', 2: '弁護士', 3: '泥棒', 4: '庭師', 5: ' マジシャン',
-           6: '冒険家', 7: '傭兵', 8: '空軍', 9: '祭司', 10: '機械技師',
-           11: ' オフェンス', 12: '心眼', 13: '調香師', 14: 'カウボーイ', 15: '踊り子',
-           16: ' 占い師', 17: '納棺師', 18: '探鉱者', 19: '呪術師', 20: '野人',
-           21: '曲芸師', 22: '一等航海士', 23: 'バーメイド', 24: 'ポストマン', 25: '墓守',
-           26: '｢囚人｣', 27: '昆虫学者', 28: '画家', 29: 'バッツマン', 30: '玩具職人',
-           31: '患者', 32: '｢心理学者｣', 33: '小説家', 34: '｢少女｣', 35: '泣きピエロ',
-           36: '教授', 37: '骨董商', 38: '作曲家', 39: '記者', 40: '航空エンジニア',
-           41: '応援団', 42: '人形師', 43: '火災調査員', 44: '｢レディ・ファウロ｣', 45: '｢騎士｣',
-           46: '気象学者', 47: '弓使い', 48: '｢脱出マスター｣', 49: '幸運児'}
-banA=st.selectbox("1人目のBAN済サバイバーを選択",options=list(survivors.values()))
-banB=st.selectbox("2人目のBAN済サバイバーを選択",options=list(survivors.values()))
-banC=st.selectbox("3人目のBAN済サバイバーを選択",options=list(survivors.values()))
-#書き込む前に並べ替え
-selected_survivor=[banA,banB,banC]
-sorted_ban=sorted(selected_survivor,key=lambda x:survivors.get(x,x))
-ban1=sorted_ban[0]
-ban2=sorted_ban[1]
-ban3=sorted_ban[2]
-
 #入力フォーム_マップ
 maps=["","軍需工場","赤の教会","聖心病院","湖景村","月の河公園","レオの思い出","永眠町","中華街","罪の森"]
 map=st.selectbox("マップを選択",options=maps)
+
+#入力フォーム_サバイバー
+#見やすさのためにたたむ
+with st.expander("BANされたサバイバーを３人記録"):
+    survivors= {0: '', 1: '医師', 2: '弁護士', 3: '泥棒', 4: '庭師', 5: 'マジシャン',
+                6: '冒険家', 7: '傭兵', 8: '空軍', 9: '祭司', 10: '機械技師',
+                11: 'オフェンス', 12: '心眼', 13: '調香師', 14: 'カウボーイ', 15: '踊り子',
+                16: '占い師', 17: '納棺師', 18: '探鉱者', 19: '呪術師', 20: '野人',
+                21: '曲芸師', 22: '一等航海士', 23: 'バーメイド', 24: 'ポストマン', 25: '墓守',
+                26: '｢囚人｣', 27: '昆虫学者', 28: '画家', 29: 'バッツマン', 30: '玩具職人',
+                31: '患者', 32: '｢心理学者｣', 33: '小説家', 34: '｢少女｣', 35: '泣きピエロ',
+                36: '教授', 37: '骨董商', 38: '作曲家', 39: '記者', 40: '航空エンジニア',
+                41: '応援団', 42: '人形師', 43: '火災調査員', 44: '｢レディ・ファウロ｣', 45: '｢騎士｣',
+                46: '気象学者', 47: '弓使い', 48: '｢脱出マスター｣', 49: '幸運児'}
+    banA=st.selectbox("1人目のBAN済サバイバー",options=list(survivors.values()))
+    banB=st.selectbox("2人目のBAN済サバイバー",options=list(survivors.values()))
+    banC=st.selectbox("3人目のBAN済サバイバー",options=list(survivors.values()))
+#書き込む前に並べ替え
+    selected_survivor=[banA,banB,banC]
+    sorted_ban=sorted(selected_survivor,key=lambda x:survivors.get(x,x))
+    ban1=sorted_ban[0]
+    ban2=sorted_ban[1]
+    ban3=sorted_ban[2]
+
+#入力フォーム_スポーン位置
+#任意・試験的であることを明記
+with st.expander("【試験中】スポーン記録(任意)"):
+    #ハンターの位置
+    with st.container():
+        if map=="永眠町":
+            sp1,sp2,sp3,sp4=st.columns(4)
+            sp5,sp6,sp7=st.columns(3)
+            sp8,sp9,sp10=st.columns(3)
+            st.session_state["spawn_h"] = st.radio("ハンタースポーン",(sp1,sp2,sp3,sp4,sp5,sp6,sp7,sp8,sp9,sp10))
+
+
 
 #入力フォーム_対戦ハンター
 hunters={0: '', 1: '復讐者', 2: '道化師', 3: '断罪狩人', 4: 'リッパー', 5: '結魂者',
@@ -72,13 +100,16 @@ hunters={0: '', 1: '復讐者', 2: '道化師', 3: '断罪狩人', 4: 'リッパ
          26: '夜の番人', 27: 'オペラ歌手', 28: '「フールズ・ゴールド」', 29: '時空の影', 30: '「足萎えの羊」',
          31: '「フラバルー」', 32: '雑貨商', 33: '「ビリヤードプレイヤー」'} 
 hunter=st.selectbox("対戦ハンターを選択",options=list(hunters.values()))
-banned_hunterA=st.selectbox("1人目のBAN済ハンターを選択",options=list(hunters.values()))
-banned_hunterB=st.selectbox("2人目のBAN済ハンターを選択",options=list(hunters.values()))
+#入力フォーム_BAN済ハンター
+#BAN済ハンターは任意なのでたたむ
+with st.expander("BAN済みハンターを記録(任意)"):
+    banned_hunterA=st.selectbox("1人目のBAN済ハンター",options=list(hunters.values()))
+    banned_hunterB=st.selectbox("2人目のBAN済ハンター",options=list(hunters.values()))
 #並べ替え
-selected_hunter=[banned_hunterA,banned_hunterB]
-sorted_ban=sorted(selected_hunter,key=lambda x:hunters.get(x,x))
-banned_hunter1=sorted_ban[0]
-banned_hunter2=sorted_ban[1]
+    selected_hunter=[banned_hunterA,banned_hunterB]
+    sorted_ban=sorted(selected_hunter,key=lambda x:hunters.get(x,x))
+    banned_hunter1=sorted_ban[0]
+    banned_hunter2=sorted_ban[1]
 
 #データ表示
 if st.button("検索"):
@@ -112,11 +143,12 @@ if st.button("検索"):
         else:
             st.text("該当なし")
         #１キャラ一致
-        st.text("【１キャラ一致】")
-        if match1chara!=[]:
-            st.table(match1chara)
-        else:
-            st.text("該当なし")
+        #データが増えたので一時削除
+        #st.text("【１キャラ一致】")
+        #if match1chara!=[]:
+        #    st.table(match1chara)
+        #else:
+        #    st.text("該当なし")
     else:
         st.warning("３人入力してください")
 
@@ -135,25 +167,3 @@ if st.button("記録"):
         st.success("記録完了")
     else:
         st.warning("未入力の項目があります")
-
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
