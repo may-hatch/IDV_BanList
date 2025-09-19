f912e41b40c9760f9636c9d94eae612742d62492
may-hatch <k025c2090@g.neec.ac.jp>
2025-09-19 10:36:00 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -164,25 +164,25 @@ with st.expander("【作業中】スポーン記録(任意)"):
     #ハンターの位置
     with st.container():
         if map=="永眠町":
-            for sp in sp_list[:11]:
-                if st.button(f"{sp}",key=f"{sp}"):
-                    st.session_state["spaw_h"]=sp
             sp1,sp2,sp3,sp4=st.columns(4)
             sp5,sp6,sp7=st.columns(3)
             sp8,sp9,sp10=st.columns(3)
-
-        elif map=="湖景村" or map=="月の河公園":
-            for sp in sp_list:
+            for sp in sp_list[:11]:
                 if st.button(f"{sp}",key=f"{sp}"):
                     st.session_state["spaw_h"]=sp
+
+        elif map=="湖景村" or map=="月の河公園":
             sp1,sp2,sp3,sp4=st.columns(4)
             sp5,sp6,sp7,sp8=st.columns(4)
             sp9,sp10,sp11,sp12=st.columns(4)
-
-        else:
-            for sp in sp_list[:9]:
+            for sp in sp_list:
                 if st.button(f"{sp}",key=f"{sp}"):
                     st.session_state["spaw_h"]=sp
+
+        else:
             sp1,sp2,sp3=st.columns(3)
             sp4,sp5,sp6=st.columns(3)
             sp7,sp8,sp9=st.columns(3)
+            for sp in sp_list[:9]:
+                if st.button(f"{sp}",key=f"{sp}"):
+                    st.session_state["spaw_h"]=sp
