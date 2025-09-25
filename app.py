52bc0cd43932c83a15584f62c45780faddd6c0bb
may-hatch <k025c2090@g.neec.ac.jp>
2025-09-25 11:36:56 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ with st.expander("使い方"):
     """)
 
 #統計表示
-if st.button("統計を表示（）"):
+if st.button("統計を表示（テスト中）"):
     response_all=supabase.table("BannedCharaList").select("hunter").execute()
     records_all=response_all.data
 
@@ -68,18 +68,18 @@ if st.button("統計を表示（）"):
     sorted_items = sorted(hunter_ratio.items(), key=lambda x: x[1], reverse=True)
     labels = [item[0] for item in sorted_items]
     values = [item[1] for item in sorted_items]
+    st.write(sorted_items)
 
-# グラフ描画
-    fig, ax = plt.subplots()
-    ax.bar(labels, values)
-    ax.set_ylabel("割合（％）")
-    ax.set_title("ハンター遭遇率")
-    plt.xticks(rotation=45)
-
+#pltでグラフ描画
+#    fig, ax = plt.subplots()
+#    ax.bar(labels, values)
+#    ax.set_ylabel("割合（％）")
+#    ax.set_title("ハンター遭遇率")
+#    plt.xticks(rotation=45)
 # Streamlitで表示
-    st.pyplot(fig)
+#    st.pyplot(fig)
 
-
+#pandasでつくって表示
 #    df=pd.DataFrame({
 #        "ハンター":list(hunter_ratio.keys()),
 #        "割合(％)":list(hunter_ratio.values())
